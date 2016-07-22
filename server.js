040f6fe7619d1afe8d4d20808c2b628393be1131
Leandro Machado Costa <leandro.costa@MPB.eng.br>
2016-07-22 15:09:58 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+/*Define dependencies.*/
+
+var express=require("express");
+var jsonfile = require('jsonfile')
+var bodyParser = require('body-parser');
+var path = require('path');
+var app=express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json() ); 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/ramais', function(req, res) {
+	res.sendFile(__dirname + req.route.path+"//index.html");
+});
+
+
+app.post('/salvar',function(req,res){
+    if (!req.body) return res.sendStatus(400)
+    jsonfile.writeFileSync(__dirname +'//public//ramais//contatos.json', req.body.lista)
+    res.end();
+});
+
+app.get('/',function(req,res){
+
+    var hostLista = req.headers.host.split('.'); 
+
+    res.redirect('/'+hostLista[0]);
+
+});
+
+
+
+/*Run the server.*/
+app.listen(80,function(){
+    console.log("Working on port 3000");
+});
